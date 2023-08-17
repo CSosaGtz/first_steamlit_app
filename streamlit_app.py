0c226fcc2babec2e86fb0969207ab35eedaf42b2
CSosaGtz <97200765+CSosaGtz@users.noreply.github.com>
2023-08-17 14:42:36 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,3 +23,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+# panda tool to read through the received data from the api call
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# writes the data previously read into a dataframe using pandas
+streamlit.dataframe(fruityvice_normalized)
